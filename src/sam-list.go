636e600063f6355c72ebb16249033351d5a7e20a
idk <problemsolver@openmailbox.org>
2017-09-19 23:30:09 -0400

    improved the samStack, but I'm going to bed. I'll build the rest of the named pipes part tomorrow

--- src/sam-list.go
+++ src/sam-list.go
@@ -1,13 +1,67 @@
 package main
 
 import (
+        "bufio"
+        "path/filepath"
         "fmt"
+        "log"
+        "os"
+        "syscall"
 )
 
 type samList struct{
         stackOfSams []samHttp
+        err error
+
         samAddrString string
         samPortString string
+
+        sendPath string
+        sendPipe *os.File
+        sendBuff bufio.Reader
+
+        recvPath string
+        recvPipe *os.File
+}
+
+func (samStack * samList) initPipes(){
+        pathConnectionExists, err := exists(filepath.Join(connectionDirectory, "parent"))
+        samStack.checkErr(err)
+        if ! pathConnectionExists {
+                fmt.Println("Creating a connection:", "parent")
+                os.Mkdir(filepath.Join(connectionDirectory, "parent"), 0755)
+        }
+
+        samStack.sendPath = filepath.Join(connectionDirectory, "parent", "send")
+        pathSendExists, sendErr := exists(samStack.sendPath)
+        samStack.checkErr(sendErr)
+        if ! pathSendExists {
+                samStack.err = syscall.Mkfifo(samStack.sendPath, 0755)
+                fmt.Println("Preparing to create Pipe:", samStack.sendPath)
+                samStack.checkErr(samStack.err)
+                fmt.Println("checking for problems...")
+                samStack.sendPipe, samStack.err = os.OpenFile(samStack.sendPath , os.O_RDWR|os.O_CREATE, 0755)
+                fmt.Println("Opening the Named Pipe as a File...")
+                samStack.sendBuff = *bufio.NewReader(samStack.sendPipe)
+                fmt.Println("Opening the Named Pipe as a Buffer...")
+                fmt.Println("Created a named Pipe for sending requests:", samStack.sendPath)
+        }
+
+        samStack.recvPath = filepath.Join(connectionDirectory, "parent", "recv")
+        pathRecvExists, recvErr := exists(samStack.recvPath)
+        samStack.checkErr(recvErr)
+        if ! pathRecvExists {
+                samStack.err = syscall.Mkfifo(samStack.recvPath, 0755)
+                fmt.Println("Preparing to create Pipe:", samStack.recvPath)
+                samStack.checkErr(samStack.err)
+                fmt.Println("checking for problems...")
+                samStack.recvPipe, samStack.err = os.OpenFile(samStack.recvPath , os.O_RDWR|os.O_CREATE, 0755)
+                fmt.Println("Created a named Pipe for recieving responses:", samStack.recvPath)
+        }
+}
+
+func (samStack *samList) createClient(samAddrString string, samPortString string){
+
 }
 
 func (samStack *samList) clientLoop(){
@@ -19,33 +73,18 @@ func (samStack *samList) clientLoop(){
         }
 }
 
-func (samStack *samList) clientAdd(request string){
-        //append(samStack.stackOfSams, newSamHttp(samList.samAddrString, samList.samPortString, request))
-        //samStack.clientCheckExists(request)
+func (samStack *samList) cleanupClient(){
+        samStack.sendPipe.Close()
+        samStack.recvPipe.Close()
+        //samStack.sam.Close()
+        //os.RemoveAll(filepath.Join(connectionDirectory, samStack.host))
 }
 
-func (samStack *samList) clientCheckExists(request string) (bool, int) {
-        for samIndex, samInst := range samStack.stackOfSams {
-                if samInst.hostCheck( samInst.hostSet(request) ) {
-                        fmt.Println(request)
-                        return true, samIndex
-                }
-        }
-        return false, -1
-}
-
-func (samStack *samList) readInputPipe(){
-        request := "http://i2p-projekt.i2p/en/docs./api/samv3"
-        clientExistsAlready, clientIndex := samStack.clientCheckExists(request)
-        if clientExistsAlready {
-                samStack.stackOfSams[clientIndex].sendRequest(request)
-        }else{
-                samStack.clientAdd(request)
-        }
-}
-
-func (samStack *samList) createClient(){
-
+func (samStack *samList) checkErr(err error) {
+	if err != nil {
+                samStack.cleanupClient()
+		log.Fatal(err)
+	}
 }
 
 func createSamList(samAddr string, samPort string) samList{
