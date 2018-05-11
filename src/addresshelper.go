5a487ea71dba5d5159427d4f15cdac928bca2932
idk <problemsolver@openmailbox.org>
2018-05-11 02:26:08 -0400

    I need to fix that messy wrapper of the dial function to use the new one from goSam. I'll do it at work tomorrow.

--- src/addresshelper.go
+++ src/addresshelper.go
@@ -182,6 +182,7 @@ func (addressBook *addressHelper) updateAh() {
 
 func newAddressHelper(addressHelperUrl, samHost, samPort string) *addressHelper {
 	var a addressHelper
+	//a.assistant = i2paddresshelper.NewI2pAddressHelperFromOptions(i2paddresshelper.SetJump(addressHelperUrl), i2paddresshelper.SetAddr(samHost), i2paddresshelper.SetPort(samPort))
 	a.assistant = i2paddresshelper.NewI2pAddressHelper(addressHelperUrl, samHost, samPort)
 	log.Println("addresshelper.go connecting to SAM bridge on:", addressHelperUrl, samHost, ":", samPort)
 	a.pairs = []string{}
